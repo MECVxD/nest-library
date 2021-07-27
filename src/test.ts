09176d29315065d93e534f5014c55c2733f74717
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2021-07-27 00:17:25 -0500

    Add files

---
+++ src/test.ts
@@ -0,0 +1,11 @@
+export function getHello(name: string): string {
+  return 'Hola' + name;
+}
+
+export function suma(x: number, y: number): number {
+  return x + y;
+}
+
+export function resta(x: number, y: number): number {
+  return x - y;
+}
